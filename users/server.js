c2a4742aaf8e434dcf17f1040a3aadedfee77311
KantKontrol <nick.derissio.tech@gmail.com>
2021-01-19 17:06:01 -0500

    setup graphql to be used by express

--- users/server.js
+++ users/server.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const expressGraphQL = require("express-graphql");
 
 
 const PORT = process.env.PORT || 3001;
 
 
 const app = express();
+
+app.use("/graphql", expressGraphQL({
+    graphiql: true
+}));
 
 
 app.listen(PORT, () => {
